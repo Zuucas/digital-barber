a5169ad174662ae52a7d1cb6b51e865c638242ed
Zuucas <lucasmelo402@outlook.com.br>
2024-09-17 12:02:19 -0300

    feat: add shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`dark `}
       >
         {children}
       </body>
