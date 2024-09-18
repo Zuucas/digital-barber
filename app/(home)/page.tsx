04ca76f964d1cc12f1c84d6588924914f8d03491
Zuucas <lucasmelo402@outlook.com.br>
2024-09-18 13:39:30 -0300

    feat: add welcome msg

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,24 @@
-// import Image from "next/image";
 
+
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header></Header>
+
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Lucas!</h2>
+        
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' d 'de' MMMM 'de' yyyy " , 
+          { locale: ptBR })}
+        </p>
+
+
+      </div>
     </div>
 
   );
